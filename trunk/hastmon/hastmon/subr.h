c80673cda49605e09a936d9adc74a2acccb576bf
to.my.trociny <to.my.trociny@7cc39589-3f15-7607-2336-b5ad14c7b1d2>
2010-10-16 21:28:02 +0000

    Initial version

---
+++ trunk/hastmon/hastmon/subr.h
@@ -0,0 +1,59 @@
+/*-
+ * Copyright (c) 2010 The FreeBSD Foundation
+ * Copyright (c) 2010 Mikolaj Golub <to.my.trociny@gmail.com>
+ * All rights reserved.
+ *
+ * This software was developed by Mikolaj Golub. The source is derived
+ * from HAST developed by Pawel Jakub Dawidek under sponsorship from
+ * the FreeBSD Foundation.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef	_SUBR_H_
+#define	_SUBR_H_
+
+#include <sys/types.h>
+#include <stdbool.h>
+
+#include "hast.h"
+
+#define	KEEP_ERRNO(work)	do {					\
+	int _rerrno;							\
+									\
+	_rerrno = errno;						\
+	work;								\
+	errno = _rerrno;						\
+} while (0)
+
+const char *role2str(int role);
+const char *state2str(int state);
+
+int complaints_add(struct hast_resource *res);
+int complaints_cnt(struct hast_resource *res);
+void complaints_clear(struct hast_resource *res);
+
+int check_resource(struct hast_resource *res);
+
+#endif	/* !_SUBR_H_ */
