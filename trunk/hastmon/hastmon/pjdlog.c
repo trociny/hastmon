8a1cd5fb210a11d0565b46858038383ebb3c42ff
to.my.trociny <to.my.trociny@7cc39589-3f15-7607-2336-b5ad14c7b1d2>
2011-01-30 17:34:11 +0000

    Initialize all global variables on pjdlog_init().
    Taken from HAST (r218040).

--- trunk/hastmon/hastmon/pjdlog.c
+++ trunk/hastmon/hastmon/pjdlog.c
@@ -42,8 +42,7 @@
 #include "pjdlog.h"
 
 static bool pjdlog_initialized = false;
-static int pjdlog_mode = PJDLOG_MODE_STD;
-static int pjdlog_debug_level = 0;
+static int pjdlog_mode, pjdlog_debug_level;
 static char pjdlog_prefix[128];
 
 void
@@ -56,6 +55,8 @@ pjdlog_init(int mode)
 	if (mode == PJDLOG_MODE_SYSLOG)
 		openlog(NULL, LOG_PID | LOG_NDELAY, LOG_DAEMON);
 	pjdlog_mode = mode;
+	pjdlog_debug_level = 0;
+	bzero(pjdlog_prefix, sizeof(pjdlog_prefix));
 
 	pjdlog_initialized = true;
 }
