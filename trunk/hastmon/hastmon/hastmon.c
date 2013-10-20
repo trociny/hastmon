eb0458068f3c59898c75fc99ef4ccf023393833d
to.my.trociny <to.my.trociny@7cc39589-3f15-7607-2336-b5ad14c7b1d2>
2013-10-20 16:09:41 +0000

    Make the log message less confusing.

--- trunk/hastmon/hastmon/hastmon.c
+++ trunk/hastmon/hastmon/hastmon.c
@@ -839,15 +839,18 @@ listen_accept(void)
 	}
 
 	/*
-	 * If the resource is primary but the request comes from the
-	 * resource with higher priority (lower number) we have to
-	 * switch to previous role. If our priority is higher it is
-	 * expected that other end will switch to secondary/init.
+	 * If the resource is primary but the request comes from the resource
+	 * with higher or equal priority (lower number) we have to switch to
+	 * the previous role.  If our priority is higher it is expected that
+	 * other end will switch to secondary/init.
 	 */
 	if (res->hr_role == HAST_ROLE_PRIMARY) {
 		if (res->hr_priority >= nv_get_int32(nvin, "priority")) {
-			pjdlog_debug(1, "Request has come for resource %s from primary with higher priority (priority number %d, our is %d).",
-			    res->hr_name, nv_get_int32(nvin, "priority"), res->hr_priority);
+			pjdlog_debug(1, "Request has come for resource %s "
+			    "from primary with higher or equal priority "
+			    "(priority number %d, our is %d).",
+			    res->hr_name, nv_get_int32(nvin, "priority"),
+			    res->hr_priority);
 			if (res->hr_workerpid != 0) {
 				pjdlog_debug(1,
 				    "Worker process exists (pid=%u), stopping it.",
